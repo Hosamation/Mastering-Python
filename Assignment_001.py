0ceaf8422d70c31d68f8e26695705609a3a6e27c
MoHosam <hosamation15@gmail.com>
2020-11-17 13:39:55 +0200

    Write A Multiple Line Comment Describe The Comment Uses

---
+++ Assignment_001.py
@@ -0,0 +1,17 @@
+# -------------------------------------------------------
+# --- Write A Multiple Line Comment Describe The Page ---
+# -------------------------------------------------------
+
+
+# -------------------------------
+# Informations About File
+# License
+# Who Created The File
+# When The File Created
+# Why The File Created
+# Write Beside The Programming Line
+# Write Before The Programming Line
+# Prevent Code From Run
+# -------------------------------
+
+# Keyboard Shortcut => ctrl + /
