21159b8d125aeb444792273477a5393e05c0e015
MoHosam <hosamation15@gmail.com>
2020-11-17 15:46:02 +0200

    Write Comment & Create Variables & Print Data Type, Separator & Concatenate

--- Assignment_001.py
+++ Assignment_001.py
@@ -1,8 +1,13 @@
-# -------------------------------------------------------
-# --- Write A Multiple Line Comment Describe The Page ---
-# -------------------------------------------------------
+# ---------------------------------------------------------
+# ------------  Assignment For Lessons 1 - 10  ------------
+# ---------------------------------------------------------
 
+# ---------------------------------------------------------
+# ---- Write A Multiple Line Comment Describe The Page ----
+# ---------------------------------------------------------
 
+# [01] Write A Multiple Line Comment Describe The Page
+# For What you are using the comments?!
 # -------------------------------
 # Informations About File
 # License
@@ -13,5 +18,42 @@
 # Write Before The Programming Line
 # Prevent Code From Run
 # -------------------------------
-
 # Keyboard Shortcut => ctrl + /
+
+####################################################
+
+# [02] Create 3 Variables ( Name => String, Age => Number, Country => String )
+
+####################################################
+###### There are 3 ways to type the variables ######
+# --------------------------------------------------
+# ------- Every single variable in one line --------
+Name = 'Mohammad'       # String
+Age = 33                # Integer
+Country = 'Egypt'       # String
+# --------------------------------------------------
+####################################################
+# --------------------------------------------------
+# --------- In one line using semicolon ; ----------
+# Name = 'Mohammad' ; Age = 33 ; Country = 'Egypt'
+# --------------------------------------------------
+####################################################
+# --------------------------------------------------
+# ----------- Multi Assign using comma , -----------
+Name, Age,  Country = 'Mohammad', 33, 'Egypt'
+# Name = 'Mohammad' , Age = 33 , Country = 'Egypt
+# --------------------------------------------------
+
+# [03] Print To The Console 3 Lines Contains Created Variables Type
+# [04] Write Single Line Comment Before Every Variable Describe The Data Type
+
+print(type(Name))         # <class 'str'> String
+print(type(Age))          # <class 'int'> Integer
+print(type(Country))      # <class 'str'> String
+
+# [05] Print A Separator Contains 20 # Character
+print('#' * 20)     # Output ####################
+
+# [06] Concatenate & Print "Hello {Name Var} Your Age Is {Age Var} & Country Is {Country Var}"
+# Hello Mohammad Your Age is 33 & Country Is Egypt
+print(f'Hello {Name} Your Age is {Age} & Country Is {Country}')
